80fe3d25db85e509ed5d6db9ee288a19d3a5c9d0
YuvrajS24 <yuvraj.sachdev2403@gmail.com>
2026-08-19 12:44:48 +0530

    Remove retired and sidebar-removed screens from page access configuration checklist

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,13 +4,9 @@ export const AVAILABLE_PAGES = [
   'Generate PO',
   'Create Bill',
   'Ready Product',
-  'Check Transport',
-  'Print Invoice',
   'Supply Check',
   'Approve Product',
-  'Payment Processing',
-  'Canceled Orders',
-  'Deleted POs'
+  'Payment Processing'
 ];
 export const AVAILABLE_ROLES = ['ADMIN', 'USER'];
 
